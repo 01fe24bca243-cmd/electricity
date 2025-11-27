5d98dd57debd4568f163591f366f04ccf1aa781f
01fe24bca243-cmd <01fe24bca243@kletech.ac.in>
2025-11-27 14:45:18 +0530

    Create electricity.py

---
+++ electricity.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) == 2:
+    units = float(sys.argv[1])
+else:
+    print("No units entered. Using default units...")
+    units = 200   # default units
+
+bill = units * 5
+
+print("Electricity Bill: ₹", bill)
